b62602b4c414b0b892bf6e5f60c351526c1e7c83
cz17691181769118 <97105686+cz17691181769118@users.noreply.github.com>
2022-01-04 21:50:26 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="shr" || userName=="史惠睿"  &&  pwd=="19980105" || pwd=="199815"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Wrong Password");
+	}
+});
